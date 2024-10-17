64ceb6e68cb18e0a12c7990be977d5c55c45b9cc
seungjae915 <seungjae915@naver.com>
2024-10-17 15:19:13 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace G24W07WFCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAdd = new Button();
+            labelCount = new Label();
+            SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(12, 373);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(184, 65);
+            btnAdd.TabIndex = 0;
+            btnAdd.Text = "증가";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += OnAdd;
+            // 
+            // labelCount
+            // 
+            labelCount.BackColor = SystemColors.Info;
+            labelCount.Font = new Font("맑은 고딕", 36F);
+            labelCount.Location = new Point(12, 9);
+            labelCount.Name = "labelCount";
+            labelCount.Size = new Size(382, 343);
+            labelCount.TabIndex = 1;
+            labelCount.Text = "0";
+            labelCount.TextAlign = ContentAlignment.MiddleCenter;
+            labelCount.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(406, 450);
+            Controls.Add(labelCount);
+            Controls.Add(btnAdd);
+            Name = "Form1";
+            Text = "카운터";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnAdd;
+        private Label labelCount;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,23 @@
+namespace G24W07WFCounter
+{
+    public partial class Form1 : Form
+    {
+        private int Count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnAdd(object sender, EventArgs e)
+        {
+            //labelCount.Text = (++Count).ToString();
+            //labelCount.Text = "" + ++Count;
+            labelCount.Text = $"{++Count}";
+        }
+    }
+}
